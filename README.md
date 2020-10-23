48f73e69f6f6fc7714a2bfa08d33ae59497c6563
Steven Eichten <u5483348@RSB0001341.local>
2020-10-22 20:40:53 -0500

    first pass implementation

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 # inky-covid
 A covid dashboard for the inky pHAT
 
+![drawing](https://i.imgur.com/ClLXJvZ.jpeg=250x)
 # installation
 
+This used python 3.7 and the (inky-phat libraries [inky-phat libraries](https://github.com/pimoroni/inky-phat)
+
+```
+pip3 install -r requirements.txt
+```
+
 # usage
+
+```
+usage: inky-covid.py [-h] [--state state] [--flip flip]
+
+inky-pHAT dashboard of daily COVID-19 positives
+
+optional arguments:
+  -h, --help     show this help message and exit
+  --state state  two-letter US state abbreviation
+  --flip flip    flip the display 180 degrees
+```
 
 # license
 GPL-3
--- inky-covid.py
+++ inky-covid.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageDraw, ImageFont
 import argparse, io
 from datetime import datetime, timedelta
 from matplotlib import font_manager
-
+from inkyphat import RED, BLACK, text, set_image, set_rotation, show
 # Create the parser
 my_parser = argparse.ArgumentParser(description='inky-pHAT dashboard of daily COVID-19 positives')
 
@@ -34,7 +34,7 @@ x = df["date"]
 y = df["positiveIncrease"]
 myFmt = mdates.DateFormatter('%m-%d')
 current=x[0]
-
+last_change=y[0]
 fig, ax = plt.subplots()
 ax.plot(x,y)
 ax.xaxis.set_major_formatter(myFmt)
@@ -45,23 +45,23 @@ w, h = (212, 104)
 dpi = 144
 fig, ax = plt.subplots(figsize=(212/dpi, 104/dpi), dpi=dpi)
 
-fig.subplots_adjust(top=1, bottom=0, left=0.15, right=.95)
+fig.subplots_adjust(top=1, bottom=0, left=0.45, right=.95)
 
 ticks_font = font_manager.FontProperties(fname='font1.TTF', size=4)
 plt.rcParams['text.antialiased'] = False
 for label in ax.get_yticklabels() :
     label.set_fontproperties(ticks_font)
-ax.yaxis.set_tick_params(pad=2, width=1)
+ax.yaxis.set_tick_params(pad=1, width=1)
 ax.xaxis.set_ticks([])
 ax.set_frame_on(False)
-ax.plot(x,y,marker="o",markersize=2)
+ax.plot(x,y) #,marker="o",markersize=2)
 ax.xaxis.set_major_formatter(myFmt)
 fig.autofmt_xdate()
 ax.autoscale_view()
 
 ymin, ymax = ax.get_ylim()
 
-font = ImageFont.truetype('font1.TTF', 16)
+font = ImageFont.truetype('Cantarell-Bold.ttf', 16)
 
 with io.BytesIO() as f:
     fig.savefig(f, dpi=dpi, cmap="bwr", interpolation="none", origin="lower", pad_inches=0)
@@ -70,15 +70,17 @@ with io.BytesIO() as f:
     d = ImageDraw.Draw(i)
 
 
-    from inkyphat import RED, BLACK, text, set_image, set_rotation, show
+
     set_image(i)
     if args.flip:
         set_rotation(180)
 
     #text((60, 25), '{:.0f}'.format(last_change), BLACK, font)
-    text((0, 90), "MN COVID+", BLACK, font)
-    text((100,90),'{}: {:.0f}'.format(x[0].strftime('%m-%d'),last_change), RED, font)
-    #i.save('test.tif')
-    #raise SystemExit
+    text((0, 0), "{}".format(args.state.upper()), BLACK, font)
+    text((0,15),"COVID+",RED, font)
+    text((0,30),'{}:'.format(x[0].strftime('%m-%d')),BLACK, font)
+    text((0,45),'{:.0f}'.format(last_change), RED, font)
+    text((0,60),'NEW',BLACK, font)
+    text((0,75),'CASES',BLACK, font)
 
     show()
